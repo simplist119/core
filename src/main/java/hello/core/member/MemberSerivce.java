cb35e815104ac2853ce7e6a2355cd5d538dfdbe3
simplist119 <simplist119@gmail.com>
2024-10-29 15:31:06 +0900

    회원 도메인 개발

--- src/main/java/hello/core/member/MemberSerivce.java
+++ src/main/java/hello/core/member/MemberSerivce.java
@@ -1,2 +1,8 @@
-package hello.core.member;public interface MemberSerivce {
+package hello.core.member;
+
+public interface MemberSerivce {
+
+    void join(Member member);
+
+    Member findMember(Long memberId);
 }
